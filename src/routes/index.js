f7fc075e82fca6baf4daf2f02f4da59a85c07eae
Fred <1988.fred@gmail.com>
2024-07-17 16:37:06 +0900

    Feat: add 24h cache for all responses

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 // Route pour servir le fichier HTML statique
 router.get('/', (req, res) => {
+    res.set('Cache-Control', 'public, max-age=86400')
     res.sendFile(path.join(__dirname, '../../public/index.html'))
 })
 
--- src/routes/publications.js
+++ src/routes/publications.js
@@ -4,6 +4,9 @@ const router = express.Router()
 const publicationsController = require('../controllers/publications')
 
 // Route pour les publications
-router.get('/', publicationsController.getAllPublications)
+router.get('/', (req, res, next) => {
+    res.set('Cache-Control', 'public, max-age=86400')
+    next()
+}, publicationsController.getAllPublications)
 
 module.exports = router
