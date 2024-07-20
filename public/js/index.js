017a2b3c0becd46662bf8e677e8148cd0c98e54e
Fred <1988.fred@gmail.com>
2024-07-20 12:11:30 +0900

    Style responsive

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,40 @@
-function init() {
-    const $hamBtn = document.getElementById('🍔-btn')
-    $hamBtn.addEventListener('click', () => {
-        const $navMobile = document.getElementById('nav-hamburger')
-        $navMobile.toggleAttribute('open')
-    })
-}
+// Mobile navigation
+const $hamBtn = document.getElementById('🍔-btn')
+$hamBtn.addEventListener('click', () => {
+    const $navMobile = document.getElementById('nav-hamburger')
+    $navMobile.toggleAttribute('open')
+})
 
-init()
+window.addEventListener('click', (event) => {
+    const $menuBtn = event.target.closest('#🍔-btn')
+    if (!$menuBtn) {
+        const $navMobile = document.getElementById('nav-hamburger')
+        $navMobile.removeAttribute('open')
+    }
+})
+
+// Scroll to top button
+// Utiliser l'Intersection Observer pour détecter quand l'élément sentinelle est visible
+let observer = new IntersectionObserver(function(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            $upBtn.style.display = "grid"
+        } else {
+            $upBtn.style.display = "none"
+        }
+    })
+})
+
+// Observer l'élément sentinelle
+observer.observe(document.getElementById("VS"))
+
+var $upBtn = document.getElementById("upBtn")
+$upBtn.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }) )
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        $upBtn.style.display = "block"
+    } else {
+        $upBtn.style.display = "none"
+    }
+}
